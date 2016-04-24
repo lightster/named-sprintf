33ab2e6d40a067f8094979620f6804f1e09c99f6
Matt Light <matt.light@illuminateed.com>
2016-04-23 18:07:05 -0700

    Implement \Lstr\Sprintf\Sprintf::sprintf()

---
+++ src/Sprintf.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Lstr\Sprintf;
+
+class Sprintf
+{
+    /**
+     * @param string $format
+     * @param array $parameters
+     * @return string
+     * @throws Exception
+     */
+    public static function sprintf($format, array $parameters)
+    {
+        $named_param_map = self::getNamedParamMap($parameters);
+
+        $replacement_sets = [];
+        $matches = [];
+        $pattern = '
+            /
+                (
+                    (?:^|[^%])
+                    (?:%%)*%
+                ) # 1: the percent sign(s)
+                \( # literal open paren
+                    ([a-zA-Z_][a-zA-Z0-9_\-]*?) # 2: the name of the param 
+                \) # literal close paren
+            /mx
+        ';
+        if (preg_match_all($pattern, $format, $matches, PREG_SET_ORDER)) {
+            foreach ($matches as $match) {
+                $percent_signs = $match[1];
+                $named_param = $match[2];
+
+                if (!array_key_exists($named_param, $named_param_map)) {
+                    throw new Exception(
+                        "The '{$named_param}' parameter was in the format string but was not provided"
+                    );
+                }
+
+                $replacement_sets[$match[0]] = "{$percent_signs}{$named_param_map[$named_param]}";
+            }
+        }
+
+        $searches = array_keys($replacement_sets);
+        $replacements = array_values($replacement_sets);
+        $parsed_format = str_replace($searches, $replacements, $format);
+
+        return vsprintf($parsed_format, array_values($parameters));
+    }
+
+    /**
+     * @param array $named_params
+     * @return array
+     */
+    private static function getNamedParamMap(array $named_params)
+    {
+        $param_map = [];
+        $param_number = 1;
+        foreach ($named_params as $param_name => $param_value) {
+            $param_map[$param_name] = "{$param_number}\$";
+            ++$param_number;
+        }
+
+        return $param_map;
+    }
+}
