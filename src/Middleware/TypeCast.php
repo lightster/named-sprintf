ce82e7038ca03ded4b9f5eb7772ac4521588a086
Matt Light <matt.light@lightdatasys.com>
2016-05-28 12:49:59 -0700

    Stop treating `null` default type as empty string
    If a parameter does not have a type and the type cast
    middleware was not provided a type to use as the default,
    we cannot set the parameter's type to a blank string without
    causing issues for other middleware that comes along later.

--- src/Middleware/TypeCast.php
+++ src/Middleware/TypeCast.php
@@ -17,7 +17,7 @@ class TypeCast extends AbstractInvokable
     {
         parent::__construct($invokable);
 
-        $this->default_type = (null === $default_type ? '' : $default_type);
+        $this->default_type = $default_type;
     }
 
     /**
@@ -28,6 +28,25 @@ class TypeCast extends AbstractInvokable
         $name = $params->getName();
         $name_parts = explode('::', $name, 2);
         $params->setName($name_parts[0]);
-        $params->setOption('type', (isset($name_parts[1]) ? $name_parts[1] : $this->default_type));
+
+        $type = $this->getType($name_parts);
+        if (null !== $type) {
+            $params->setOption('type', $type);
+        }
+    }
+
+    /**
+     * @param array $name_parts
+     * @return string|null
+     */
+    private function getType(array $name_parts)
+    {
+        if (isset($name_parts[1])) {
+            return $name_parts[1];
+        }
+
+        if (null !== $this->default_type) {
+            return $this->default_type;
+        }
     }
 }
